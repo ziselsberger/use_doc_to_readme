4e819d419eb16410615a9eb2d218d597867e5749
Mirjam Ziselsberger <mirjam.ziselsberger@student.uibk.ac.at>
2023-04-23 22:43:50 +0200

    feat: add files

---
+++ src/functions.py
@@ -0,0 +1,29 @@
+def mean(x: int = 1, y: int = 2) -> float:
+    """Calculate mean of x and y.
+
+    :param x: First number
+    :param y: Second number
+    :return: Mean of first number (x) and second number (y).
+    """
+    result = (x + y) / 2
+    return result
+
+
+def add(x: int = 4, y: int = 5) -> int:
+    """Add two numbers (x and y).
+
+    :param x: First number
+    :param y: Second number
+    :return: Sum of first and second number
+    """
+    return x + y
+
+
+def multiply(x: int = 6, y: int = 7) -> int:
+    """Multiply two numbers (x and y).
+
+    :param x: First number
+    :param y: Second number
+    :return: Multiply first and second number
+    """
+    return x * y
